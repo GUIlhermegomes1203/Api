b687f1709f5b39b7205564b1bc70c6356afe8004
GUIlhermegomes1203 <guigomessouza20002019@gmail.com>
2025-03-10 09:07:05 -0300

    feat: classes (Escola, Aluno, Professor)

---
+++ ApiSegundoDS/Models/Aluno.cs
@@ -0,0 +1,14 @@
+﻿namespace ApiSegundoDS.Models
+{
+    public class Aluno
+    {
+        public int Id { get; set; }
+        public string? nome { get; set; }
+        public string? rma { get; set; }
+        public string? Pai { get; set; }
+        public string? Mae { get; set; }
+        public Escola Esola { get; set; }
+        public Professor Professor { get; set; }
+
+    }
+}
---
+++ ApiSegundoDS/Models/Escola.cs
@@ -0,0 +1,9 @@
+﻿namespace ApiSegundoDS.Models
+{
+    public class Escola
+    {
+        public int Id { get; set; }
+        public string? nome { get; set; }
+        public Professor Professor { get; set; }
+    }
+}
